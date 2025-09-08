26f766c084faf15fe6e95bd86de930dc8de24073
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-09-07 23:55:11 -0600

    fix: add toBuilder to FieldProtection Lombok annotation

--- src/main/java/com/portfolio/core/domain/project/FieldProtection.java
+++ src/main/java/com/portfolio/core/domain/project/FieldProtection.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
+@Builder(toBuilder = true)
 public class FieldProtection {
     @Builder.Default
     private final Boolean description = false;
